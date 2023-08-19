9457e369ecb19e712c71d952f55a35de924bb3a2
kam <kamkam6b@hotmail.com>
2023-08-19 22:21:19 +0100

    Added one more solution for merge two sorted lists

--- (Easy)_21_Merge_two_sorted_lists.js
+++ (Easy)_21_Merge_two_sorted_lists.js
@@ -10,6 +10,8 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+
+// Solution 1:
 var mergeTwoLists = function(list1, list2) {
     let head = new ListNode()
     let result = head
@@ -42,4 +44,22 @@ var mergeTwoLists = function(list1, list2) {
     }
     
     return result.next
+}
+
+// Solution 2: DP - recursive
+var mergeTwoLists = function(list1, list2) {
+    if(list1 == null) {
+        return list2
+    }
+    if(list2 == null) {
+        return list1
+    }
+
+    if(list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    }
 }
